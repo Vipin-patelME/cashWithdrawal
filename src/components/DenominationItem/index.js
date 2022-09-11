26bf504db87fc6e78ba73dcfea1be902a8b15685
Vipin Patel <112804686+Vipin-patelME@users.noreply.github.com>
2022-09-11 15:52:33 +0530

    Update index.js

--- src/components/DenominationItem/index.js
+++ src/components/DenominationItem/index.js
@@ -1,1 +1,20 @@
 // Write your code here
+// Write your code here
+import './index.css'
+
+const DenominationButton = props => {
+  const {eachDenomination} = props
+  const {value, decreaseAmount, key} = eachDenomination
+
+  const onDecreament = () => decreaseAmount(value)
+
+  return (
+    <li>
+      <button className="button-style" type="button" onClick={onDecreament}>
+        {value}
+      </button>
+    </li>
+  )
+}
+
+export default DenominationButton
